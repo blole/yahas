69e4ae2adf4457f8e7b6ce2caa7a815975a77d16
bjorn <bjorn@bjh>
2012-11-03 17:54:28 +0530

    some more merging

--- src/namenode/NameNode.java
+++ src/namenode/NameNode.java
@@ -35,16 +35,16 @@ public class NameNode extends RemoteServer implements RemoteNameNode {
 		heartBeatReceiver = new HeartBeatReceiver(this, heartBeatPort);
 		root = new NameNodeDir();
 	}
-
+	
 	private void start() {
 		new Thread(heartBeatReceiver).start();
-
+	
 		LOGGER.info("Server Ready");
 		LOGGER.info("Listening for HeartBeats on port "
 				+ heartBeatReceiver.getPort());
-
+	
 	}
-
+	
 	@Override
 	public YAHASFile getFile(String path) throws RemoteException, RemoteFileNotFoundException, RemoteFileAlreadyOpenException {
 		NameNodeFile file = root.getFile(path);
@@ -60,7 +60,7 @@ public class NameNode extends RemoteServer implements RemoteNameNode {
 		int lastSlashIndex = path.lastIndexOf('/');
 		if (lastSlashIndex < 0)
 			lastSlashIndex = 0;
-
+		
 		String dirs = path.substring(0, lastSlashIndex);
 		String name = path.substring(lastSlashIndex);
 		NameNodeFile file = new NameNodeFile(this, name, replicationFactor);
@@ -86,14 +86,14 @@ public class NameNode extends RemoteServer implements RemoteNameNode {
 		int i;
 		do {
 			i = randomIDgenerator.nextInt();
-		} while (i < 1);
+		} while (i<1);
 		return i;
 	}
 
 	@Override
 	public void blockReceived(long blockId) throws RemoteException {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
@@ -119,37 +119,42 @@ public class NameNode extends RemoteServer implements RemoteNameNode {
 		return 0;
 	}
 
+	
+	
+	
 	public void dataNodeConnected(DataNodeImage dataNodeImage) {
 		connectedDataNodes.add(dataNodeImage);
 		System.out.printf("%s connected\n", dataNodeImage);
-
-		// TODO: spawn this as a new thread.
-		// Set<BlockImage> blocks;
-		// try {
-		// blocks = dataNodeImage.getBlocks();
-		// } catch (IOException | InvalidPacketType | UnexpectedPacketType e) {
-		// e.printStackTrace();
-		// }
+				
+		//TODO: spawn this as a new thread.
+//		Set<BlockImage> blocks;
+//		try {
+//			blocks = dataNodeImage.getBlocks();
+//		} catch (IOException | InvalidPacketType | UnexpectedPacketType e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	public void dataNodeDisconnected(DataNodeImage dataNodeImage) {
 		if (connectedDataNodes.remove(dataNodeImage))
 			System.out.printf("%s disconnected\n", dataNodeImage);
 	}
-
+	
 	private RemoteNameNode getStub() throws RemoteException {
 		return (RemoteNameNode) RMIHelper.getStub(this);
 	}
-
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
-
 		int nameNodePort = Constants.DEFAULT_NAME_NODE_PORT;
-
+		
 		RMIHelper.maybeStartSecurityManager();
 		RMIHelper.makeSureRegistryIsStarted(nameNodePort);
-
-		NameNode nameNode = new NameNode(
-				Constants.DEFAULT_NAME_NODE_HEARTBEAT_PORT);
+		
+		NameNode nameNode = new NameNode(Constants.DEFAULT_NAME_NODE_HEARTBEAT_PORT);
 		try {
 			RemoteNameNode stub = nameNode.getStub();
 			RMIHelper.rebindAndHookUnbind("NameNode", stub);
@@ -159,6 +164,7 @@ public class NameNode extends RemoteServer implements RemoteNameNode {
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
 		nameNode.start();
 	}
 }

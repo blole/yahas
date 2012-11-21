299b186f1b060c4f051b01a3f2e4963d36d52a3c
govind <govind@pgovindraj>
2012-11-21 17:23:37 +0530

    Added the Skelton Client Code along with Banner

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,11 +1,19 @@
 package client;
 
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
+import com.google.common.base.Charsets;
 import java.nio.file.NotDirectoryException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 import org.apache.log4j.Logger;
 
+
+import com.google.common.io.Files;
 import common.RMIHelper;
 import common.exceptions.AllDataNodesAreDeadException;
 import common.exceptions.BadFileName;
@@ -21,6 +29,9 @@ public class Client {
 			.getCanonicalName());
 	
 	ClientNameNodeProtocol nameNode = null;
+	
+	static RemoteDir pwd=null;
+	static RemoteDir rootDir=null;
 	
 	public Client(ClientNameNodeProtocol nameNode) {
 		this.nameNode = nameNode;
@@ -109,18 +120,25 @@ public class Client {
 			System.out.println(prefix + "-" + file.getName());
 	}
 
-	public static void main(String args[]) {
-		String host = "localhost";
-		String nameNodeAddress = "//" + host + "/NameNode";
-		RMIHelper.maybeStartSecurityManager();
-		Remote nameNode = RMIHelper.lookup(nameNodeAddress);
-		Client client = new Client((ClientNameNodeProtocol) nameNode);
-		client.createFile("/world", 2, "file data".getBytes());
-		RemoteDir rootDir;
+	
+	public static void displayBanner(){
+		try {
+			String content = Files.toString(new File("/home/govind/PhD/Lecture/SWEng/Project/Code/YAHAS3/src/Banner"), Charsets.UTF_8);
+			System.out.println(content);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void operations(Client client){
+		
+		
 		try {
 			rootDir = client.nameNode.getDir("");
-			
+			client.createFile("/world", 2, "file data at ROOT".getBytes());
 			client.createDir("/dir1/");
+			client.createFile("/dir1/world", 2, "file data inside dir1".getBytes());
 			client.createDir("/dir2/");
 			client.createDir("/dir3/");
 //			client.printDirContent("dir1");
@@ -129,7 +147,65 @@ public class Client {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+	}
+	
+	
+	public static String readLineFromConsole() throws InputMismatchException
+	{
+		Scanner in = new Scanner(System.in);  
+		String str;
+		System.out.print(">");
+		str = in.nextLine();
+		return str;
+	}
+	public static void main(String args[]) {
 		
+		displayBanner();
+		Console console = System.console();
+		
+		String host = "localhost";
+		String nameNodeAddress = "//" + host + "/NameNode";
+		RMIHelper.maybeStartSecurityManager();
+		Remote nameNode = RMIHelper.lookup(nameNodeAddress);
+		Client client = new Client((ClientNameNodeProtocol) nameNode);
+
+		while(true){
+//			System.out.println(">");
+			String command = readLineFromConsole();
+			
+			if(command.equals("ls")){
+				System.out.println("LS");
+			}
+			else if (command.equals("pwd")){
+				System.out.println("Printing PWD");
+			}
+			else if (command.split("-")[0].equals(("mkdir"))){
+				System.out.println("Making directory !!");
+			}
+			else if (command.split("-")[0].equals(("mkfile"))){
+				System.out.println("make File!!");
+			}
+			else if (command.split("-")[0].equals(("cd"))){
+				System.out.println("Changing Directory!!");
+			}
+			else if (command.split("-")[0].equals(("mkfile"))){
+				System.out.println("make File!!");
+			}
+			else if(command.equals("quit")){
+				System.out.println("Bye !!");
+				return;
+			}
+			else {
+				System.out.println("Command Not Found");
+			}
+			System.out.println("> " + command );
+			
+			
+		}
+		
+		
+				
 		
 		
 	}
